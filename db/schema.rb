ed06eca6e7cc792e7171cea1af136b0d1b09ba61
Loris Reynaud <loris.reynaud@gmail.com>
2022-01-04 17:53:41 +0100

    seeds ok but little

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_04_113945) do
+ActiveRecord::Schema.define(version: 2022_01_04_162519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,12 +21,9 @@ ActiveRecord::Schema.define(version: 2022_01_04_113945) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "family"
+    t.string "species"
     t.string "scientific_name"
-    t.string "food"
-    t.string "lifespan"
     t.string "weight"
-    t.string "living"
   end
 
   create_table "contacts", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -64,97 +64,103 @@ Animal.destroy_all
 puts "datas destroyed"
 
 puts "Creating Users..."
-User.create!(email: 'emmanuel@emmanuel.com', name: 'Emmanuel', phone_number: '890895643322', password: 'azerty')
-User.create!(email: 'loris@loris.com', name: 'Loris', phone_number: '89006789', password: 'azerty')
-User.create!(email: 'sophie@sophie.com', name: 'Sophie', phone_number: '567543221', password: 'azerty')
+User.create!(email: 'emmanuel@emmanuel.com',
+            name: 'Emmanuel',
+            password: 'azerty',
+            street: '56 rue Manu',
+            zipcode: '56000',
+            city: 'Nantes',
+            country: 'France')
+
+User.create!(email: 'sophie@sophie.com',
+            name: 'Sophie',
+            password: 'azerty',
+            street: '79 avenue soso',
+            zipcode: '43000',
+            city: 'Soso City',
+            country: 'France')
+
+User.create!(email: 'loris@loris.com',
+            name: 'Loris',
+            password: 'azerty',
+            street: '48 traverse Mars',
+            zipcode: '13006',
+            city: 'Marseille',
+            country: 'France')
+
 puts "Users ok !"
 
 puts "creating animals"
-Animal.create!(name:
-              color:
-              description:
-              family:
-              scientific_name:
-              food:
-              lifespan:
-              wheigh:
-              living:
+Animal.create!(name: "Hérisson",
+              color: "Marron",
+              description: "* Conseil 1 (ex. laisser dans le noir dans une petite boîte fermée)
+                            * Conseil 2 (ex. ne pas nourrir)
+                            * Conseil 3 (ex. ne pas abreuver)",
+              species: "Mammifère",
+              scientific_name: "Erinaceus europaeus",
+              weight: "De 0,8 à 1,5 kg",
               )
 
-Animal.create!(name:
-              color:
-              description:
-              family:
-              scientific_name:
-              food:
-              lifespan:
-              wheigh:
-              living:
+Animal.create!(name: "Mésange noire",
+              color: "Gris, noir, beige",
+              description: "* Conseil 1 (ex. laisser dans le noir dans une petite boîte fermée)
+                            * Conseil 2 (ex. ne pas nourrir)
+                            * Conseil 3 (ex. ne pas abreuver)",
+              species: "Oiseau",
+              scientific_name: "Periparus ater",
+              weight: "19-10 g"
               )
 
-Animal.create!(name:
-              color:
-              description:
-              family:
-              scientific_name:
-              food:
-              lifespan:
-              wheigh:
-              living:
+Animal.create!(name: "Moineau friquet",
+              color: "Marron, Beige, Blanc",
+              description: "* Conseil 1 (ex. laisser dans le noir dans une petite boîte fermée)
+                            * Conseil 2 (ex. ne pas nourrir)
+                            * Conseil 3 (ex. ne pas abreuver)",
+              species: "Oiseau",
+              scientific_name: "Passer montanus",
+              weight: "19 à 25 g"
               )
 
-Animal.create!(name:
-              color:
-              description:
-              family:
-              scientific_name:
-              food:
-              lifespan:
-              wheigh:
-              living:
+Animal.create!(name: "Renard roux",
+              color: "Orange, Blanc",
+              description: "* Conseil 1 (ex. laisser dans le noir dans une petite boîte fermée)
+                            * Conseil 2 (ex. ne pas nourrir)
+                            * Conseil 3 (ex. ne pas abreuver)",
+              species: "Mammifère",
+              scientific_name: "Vulpes vulpes",
+              weight: "6 à 10 kg"
               )
 
-Animal.create!(name:
-              color:
-              description:
-              family:
-              scientific_name:
-              food:
-              lifespan:
-              wheigh:
-              living:
+Animal.create!(name: "Grenouille verte",
+              color: "Vert",
+              description: "* Conseil 1 (ex. laisser dans le noir dans une petite boîte fermée)
+                            * Conseil 2 (ex. ne pas nourrir)
+                            * Conseil 3 (ex. ne pas abreuver)",
+              species: "Batracien",
+              scientific_name: "Pelophylax kl. esculentus",
+              weight: "10 à 30 g"
               )
 
-Animal.create!(name:
-              color:
-              description:
-              family:
-              scientific_name:
-              food:
-              lifespan:
-              wheigh:
-              living:
-              )
-
-Animal.create!(name:
-              color:
-              description:
-              family:
-              scientific_name:
-              food:
-              lifespan:
-              wheigh:
-              living:
-              )
-
-Animal.create!(name:
-              color:
-              description:
-              family:
-              scientific_name:
-              food:
-              lifespan:
-              wheigh:
-              living:
-              )
 puts "animal created"
+
+puts "creating contacts"
+
+Contact.create!(name: "Dr Fly",
+                speciality: "Oiseau",
+                street: "146 rue de Rome",
+                zipcode: "13006",
+                city: "Marseille",
+                country: "France",
+                phone_number: "0491456789"
+                )
+
+Contact.create!(name: "Dr Health",
+                speciality: "Batracien",
+                street: "97 rue charras",
+                zipcode: "13007",
+                city: "Marseille",
+                country: "France",
+                phone_number: "0491654321"
+                )
+
+puts "Contacts created"
