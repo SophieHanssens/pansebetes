481839adc5b624843468891a867b0ade8b4281ad
Loris Reynaud <loris.reynaud@gmail.com>
2022-01-04 14:53:51 +0100

    access to datas on signle animal page ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'nokogiri'
+
+def getAnimalPage(animal_url)
+  url = animal_url
+  animal_file = URI.open(url).read
+  animal_doc = Nokogiri::HTML(animal_file)
+
+  a_element = animal_doc.search('.animal-facts')
+    if a_element.search('dd')[6] != nil
+      p a_element.search('dd')[6].text.strip
+    else
+      p ''
+    end
+end
+
+
+url = "https://a-z-animals.com/animals/location/europe/france/"
+
+html_file = URI.open(url).read
+html_doc = Nokogiri::HTML(html_file)
+
+html_doc.search('.card').each do |element|
+  animal_url = element.search('a')[0]['href']
+
+  getAnimalPage(animal_url)
+end
