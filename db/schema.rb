083ade4a172b602c38f9ef6d920a1760bb655711
Loris Reynaud <loris.reynaud@gmail.com>
2022-01-04 12:46:19 +0100

    table animals modified

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_03_144329) do
+ActiveRecord::Schema.define(version: 2022_01_04_113945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "animals", force: :cascade do |t|
     t.string "name"
-    t.string "species"
-    t.string "latin_name"
     t.string "color"
-    t.string "size"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "family"
+    t.string "scientific_name"
+    t.string "food"
+    t.string "lifespan"
+    t.string "weight"
+    t.string "living"
   end
 
   create_table "contacts", force: :cascade do |t|
