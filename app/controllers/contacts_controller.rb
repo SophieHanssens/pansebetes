88528945f18d0239b97efe374de8065a25688d06
Loris Reynaud <loris.reynaud@gmail.com>
2022-01-05 16:13:56 +0100

    show ok

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,6 +4,27 @@ class ContactsController < ApplicationController
   end
 
   def show
+    @contact = Contact.find(params[:id])
+    @user = current_user
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
 
   end
 end
