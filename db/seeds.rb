7be365618b6b73aa140fb87f4b65537f7e469d8b
Loris Reynaud <loris.reynaud@gmail.com>
2022-01-06 11:48:19 +0100

    contact field deleted from users table

--- db/seeds.rb
+++ db/seeds.rb
@@ -70,7 +70,8 @@ User.create!(email: 'emmanuel@emmanuel.com',
             street: '56 rue Manu',
             zipcode: '56000',
             city: 'Nantes',
-            country: 'France')
+            country: 'France',
+            admin: true)
 
 User.create!(email: 'sophie@sophie.com',
             name: 'Sophie',
@@ -78,7 +79,8 @@ User.create!(email: 'sophie@sophie.com',
             street: '79 avenue soso',
             zipcode: '43000',
             city: 'Soso City',
-            country: 'France')
+            country: 'France',
+            admin: true)
 
 User.create!(email: 'loris@loris.com',
             name: 'Loris',
@@ -86,57 +88,43 @@ User.create!(email: 'loris@loris.com',
             street: '48 traverse Mars',
             zipcode: '13006',
             city: 'Marseille',
-            country: 'France')
+            country: 'France',
+            admin: true)
 
 puts "Users ok !"
 
 puts "creating animals"
 Animal.create!(name: "Hérisson",
               color: "Marron",
-              description: "* Conseil 1 (ex. laisser dans le noir dans une petite boîte fermée)
-                            * Conseil 2 (ex. ne pas nourrir)
-                            * Conseil 3 (ex. ne pas abreuver)",
-              species: "Mammifère",
+              description: "Laisser dans le noir dans une petite boîte fermée, ne pas nourrir, ne pas abreuver",
               scientific_name: "Erinaceus europaeus",
               weight: "De 0,8 à 1,5 kg",
               )
 
 Animal.create!(name: "Mésange noire",
               color: "Gris, noir, beige",
-              description: "* Conseil 1 (ex. laisser dans le noir dans une petite boîte fermée)
-                            * Conseil 2 (ex. ne pas nourrir)
-                            * Conseil 3 (ex. ne pas abreuver)",
-              species: "Oiseau",
+              description: "Laisser dans le noir dans une petite boîte fermée, ne pas nourrir, ne pas abreuver",
               scientific_name: "Periparus ater",
               weight: "19-10 g"
               )
 
 Animal.create!(name: "Moineau friquet",
               color: "Marron, Beige, Blanc",
-              description: "* Conseil 1 (ex. laisser dans le noir dans une petite boîte fermée)
-                            * Conseil 2 (ex. ne pas nourrir)
-                            * Conseil 3 (ex. ne pas abreuver)",
-              species: "Oiseau",
+              description: "Laisser dans le noir dans une petite boîte fermée, ne pas nourrir, ne pas abreuver",
               scientific_name: "Passer montanus",
               weight: "19 à 25 g"
               )
 
 Animal.create!(name: "Renard roux",
               color: "Orange, Blanc",
-              description: "* Conseil 1 (ex. laisser dans le noir dans une petite boîte fermée)
-                            * Conseil 2 (ex. ne pas nourrir)
-                            * Conseil 3 (ex. ne pas abreuver)",
-              species: "Mammifère",
+              description: "Laisser dans le noir dans une petite boîte fermée, ne pas nourrir, ne pas abreuver",
               scientific_name: "Vulpes vulpes",
               weight: "6 à 10 kg"
               )
 
 Animal.create!(name: "Grenouille verte",
               color: "Vert",
-              description: "* Conseil 1 (ex. laisser dans le noir dans une petite boîte fermée)
-                            * Conseil 2 (ex. ne pas nourrir)
-                            * Conseil 3 (ex. ne pas abreuver)",
-              species: "Batracien",
+              description: "Laisser dans le noir dans une petite boîte fermée, ne pas nourrir, ne pas abreuver",
               scientific_name: "Pelophylax kl. esculentus",
               weight: "10 à 30 g"
               )
