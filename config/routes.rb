5a6d465cc152172ecf8a2c00c66470793c45f838
Emmanuel DAVEAU <emmanuel@high-stickers.com>
2022-01-07 14:32:21 +0100

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :animals, only: %i[show index]
+  resources :animals, only: %i[show index new create]
   resources :contacts
 end
