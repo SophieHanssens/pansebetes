44dadaf6209b59a9c234f14680ea4abe7497a1f3
Emmanuel DAVEAU <emmanuel@high-stickers.com>
2022-01-10 18:00:15 +0100

    2 fields OK with IF

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -3,4 +3,21 @@ class Animal < ApplicationRecord
   belongs_to :category
 
   validates :name, presence: true
+
+  # include AlgoliaSearch
+
+  # algoliasearch do
+  #   # attributes :first_name, :last_name, :email
+  # end
+
+  # searchkick
+
+  # PG SEARCH
+  # include PgSearch::Model
+  # multisearchable against: [:name, :color]
+  # pg_search_scope :search_by_name_and_by_color,
+  #   against: [ :name, :color ],
+  #   using: {
+  #     tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  #   }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_07_141024) do
+ActiveRecord::Schema.define(version: 2022_01_10_135352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,6 +92,15 @@ ActiveRecord::Schema.define(version: 2022_01_07_141024) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["animal_id"], name: "index_favorites_on_animal_id"
     t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable"
   end
 
   create_table "species", force: :cascade do |t|
