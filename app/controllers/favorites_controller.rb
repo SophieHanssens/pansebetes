206b298a7ae4c0a6c167520992b96038d60ab7bf
Loris Reynaud <loris.reynaud@gmail.com>
2022-01-10 12:15:21 +0100

    add and destroy in db ok

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -9,8 +9,23 @@ class FavoritesController < ApplicationController
   end
 
   def create
-    favorite = Favorite.new(user_id: current_user.id, animal_id: params[:id])
-    favorite.save!
+    @contacts = Contact.all
+    @animal = Animal.find(params[:id])
+    # favorites = Favorite.all
+    # favorites.each do |favorite|
+    #   favorite_new = Favorite.new(user_id: current_user.id, animal_id: params[:id])
+    #   if favorite_new.user_id != favorite.user_id || favorite_new.animal_id != favorite.animal_id
+    #     favorite_new.save!
+    #   end
+    # end
+    favorite = Favorite.find_by(user_id: current_user.id, animal_id: params[:id])
+    if favorite.present?
+      favorite.destroy!
+    else
+      favorite_new = Favorite.new(user_id: current_user.id, animal_id: params[:id])
+      favorite_new.save!
+    end
+    redirect_to animal_path(@animal)
   end
 
 end
--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,6 +1,7 @@
 class Animal < ApplicationRecord
   has_one_attached :photo
   belongs_to :category
+  has_many :favorites
 
   validates :name, presence: true
 end
