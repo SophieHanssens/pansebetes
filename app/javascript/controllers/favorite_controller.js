1b5665f9676dd9c83fa79824cbda63da6478f6ff
Loris Reynaud <loris.reynaud@gmail.com>
2022-01-10 16:29:31 +0100

    bg color ok

--- app/javascript/controllers/favorite_controller.js
+++ app/javascript/controllers/favorite_controller.js
@@ -1,23 +1,6 @@
 import { Controller } from "stimulus"
-import { csrfToken } from "@rails/ujs";
 
 export default class extends Controller {
   static targets = ['form'];
 
-  add(e) {
-  //   e.preventDefault();
-
-  //   fetch('/favorites', {
-  //     method: 'POST',
-  //     headers: { 'Accept': "application/json", 'Content-type': "application/json", 'X-CSRF-Token': csrfToken() },
-  //     body: new FormData(this.formTarget)
-  //   })
-  //     .then(response => response.json())
-  //     .then((data) => {
-  //       console.log(data)
-  //   });
-  // }
-
-
-  }
 }
