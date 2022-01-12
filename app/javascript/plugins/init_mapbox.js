2cd0b76e33e7bd5150eaecdf4270397d6880bd77
Loris Reynaud <loris.reynaud@gmail.com>
2022-01-12 12:10:19 +0100

    full itinerary autoimplemented

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -19,23 +19,48 @@ const initMapbox = () => {
       directions.setDestination(address.textContent);
     });
 
-    var geolocate = new mapboxgl.GeolocateControl();
+    var geolocate = new mapboxgl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true
+      },
+      trackUserLocation: true,
+      showUserHeading: true,
+      showAccuracyCircle: false,
+      reverseGeocode: true
+    });
+
+    map.on('load', function () {
+      const button = document.querySelector('.mapboxgl-ctrl-geolocate');
+      console.log(button);
+      button.click();
+    });
 
     map.addControl(geolocate);
 
     geolocate.on('geolocate', function (e) {
       var lon = e.coords.longitude;
-      var lat = e.coords.latitude
+      var lat = e.coords.latitude;
       var position = [lon, lat];
       console.log(position);
-      map.on('load', function () {
-        directions.setOrigin(position); // can be address in form setOrigin("12, Elm Street, NY")
-      });
+      directions.setOrigin(position); // can be address in form setOrigin("12, Elm Street, NY")
     });
-
-
   };
 };
 // destination.textContent = address.textContent;
+
+  // var latlng = new google.maps.LatLng(lat, lng);
+  // // This is making the Geocode request
+  // var geocoder = new google.maps.Geocoder();
+  // geocoder.geocode({ 'latLng': latlng }, (results, status) => {
+  //   if (status !== google.maps.GeocoderStatus.OK) {
+  //     alert(status);
+  //   }
+  //   // This is checking to see if the Geoeode Status is OK before proceeding
+  //   if (status == google.maps.GeocoderStatus.OK) {
+  //     console.log(results);
+  //     var address = (results[0].formatted_address);
+  //   }
+  // });
+
 
 export { initMapbox };
