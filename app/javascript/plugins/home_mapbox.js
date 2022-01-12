0c3b5067a2d7bab453a2005554437927bbb676b9
Loris Reynaud <loris.reynaud@gmail.com>
2022-01-12 15:45:37 +0100

    scrolling, zoom disabled on home and show, link on card added

--- app/javascript/plugins/home_mapbox.js
+++ app/javascript/plugins/home_mapbox.js
@@ -17,6 +17,10 @@ const homeMapbox = () => {
       style: 'mapbox://styles/mapbox/streets-v10'
     });
 
+    map.scrollZoom.disable();
+    map.doubleClickZoom.disable();
+    map.addControl(new mapboxgl.NavigationControl());
+
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.info_window);
@@ -25,6 +29,29 @@ const homeMapbox = () => {
         .setLngLat([marker.lng, marker.lat])
         .setPopup(popup)
         .addTo(map);
+    });
+
+    var geolocate = new mapboxgl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true
+      },
+      trackUserLocation: true,
+      showUserHeading: true,
+      showAccuracyCircle: false,
+    });
+
+    map.on('load', function () {
+      const button = document.querySelector('.mapboxgl-ctrl-geolocate');
+      button.click();
+    });
+
+    map.addControl(geolocate);
+
+    geolocate.on('geolocate', function (e) {
+      var lon = e.coords.longitude;
+      var lat = e.coords.latitude;
+      var position = [lon, lat];
+      directions.setOrigin(position); // can be address in form setOrigin("12, Elm Street, NY")
     });
 
     fitMapToMarkers(map, markers);
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -9,6 +9,12 @@ const initMapbox = () => {
       center: [5.3699525, 43.2961743], // starting position
       zoom: 12
     });
+
+    map.scrollZoom.disable();
+    map.doubleClickZoom.disable();
+    map.addControl(new mapboxgl.NavigationControl());
+
+
     var directions = new MapboxDirections({
       accessToken: mapboxgl.accessToken
     });
@@ -26,12 +32,10 @@ const initMapbox = () => {
       trackUserLocation: true,
       showUserHeading: true,
       showAccuracyCircle: false,
-      reverseGeocode: true
     });
 
     map.on('load', function () {
       const button = document.querySelector('.mapboxgl-ctrl-geolocate');
-      console.log(button);
       button.click();
     });
 
@@ -41,26 +45,9 @@ const initMapbox = () => {
       var lon = e.coords.longitude;
       var lat = e.coords.latitude;
       var position = [lon, lat];
-      console.log(position);
       directions.setOrigin(position); // can be address in form setOrigin("12, Elm Street, NY")
     });
   };
 };
-// destination.textContent = address.textContent;
-
-  // var latlng = new google.maps.LatLng(lat, lng);
-  // // This is making the Geocode request
-  // var geocoder = new google.maps.Geocoder();
-  // geocoder.geocode({ 'latLng': latlng }, (results, status) => {
-  //   if (status !== google.maps.GeocoderStatus.OK) {
-  //     alert(status);
-  //   }
-  //   // This is checking to see if the Geoeode Status is OK before proceeding
-  //   if (status == google.maps.GeocoderStatus.OK) {
-  //     console.log(results);
-  //     var address = (results[0].formatted_address);
-  //   }
-  // });
-
 
 export { initMapbox };
