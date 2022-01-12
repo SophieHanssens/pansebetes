3086150adea1a7bda25e0bd78b11c9942c1840a1
Loris Reynaud <loris.reynaud@gmail.com>
2022-01-12 10:23:20 +0100

    geolocate ok but not auto

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -16,8 +16,24 @@ const initMapbox = () => {
     map.addControl(directions, 'top-left');
     const address = document.querySelector('#address');
     map.on('load', function () {
-      directions.setDestination(address.textContent); // can be address
+      directions.setDestination(address.textContent);
     });
+
+    var geolocate = new mapboxgl.GeolocateControl();
+
+    map.addControl(geolocate);
+
+    geolocate.on('geolocate', function (e) {
+      var lon = e.coords.longitude;
+      var lat = e.coords.latitude
+      var position = [lon, lat];
+      console.log(position);
+      map.on('load', function () {
+        directions.setOrigin(position); // can be address in form setOrigin("12, Elm Street, NY")
+      });
+    });
+
+
   };
 };
 // destination.textContent = address.textContent;
